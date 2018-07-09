85d796d3a96a25659f6f6ce0b112ff25a83a083e
Taisei Wayama <es.txw04@gmail.com>
2018-07-09 11:46:07 +0900

    FirstCommit

---
+++ DatabaseAccess0620/src/dao/HoseholdDAO.java
@@ -0,0 +1,226 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import HouseholdServlet.HouseholdServlet;
+import dto.Household;
+
+public class HoseholdDAO {
+
+	public static Household searchDao(int key){
+		//①準備
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Household result =null;
+
+
+		try {
+			// ②JDBCドライバをロードする
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// ③DBMSとの接続を確立する
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/dbdb?useSSL=false",
+					"root",
+					"1104");
+
+			// ④SQL文を作成する
+			String sql = "SELECT * FROM pokemon where Number= ? ;";
+
+			// ⑤SQL文を実行するための準備を行う
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1,key);
+
+			// ⑥SQL文を実行してDBMSから結果を受信する
+			rs = pstmt.executeQuery();
+
+			// ⑦実行結果を含んだインスタンスからデータを取り出す
+			while(rs.next() == true){
+				//ResultSetからデータを取得する
+				int Number = rs.getInt("Number");
+				String Name = rs.getString("Name");
+				String Type1 = rs.getString("Type1");
+				String Type2 = rs.getString("Type2");
+				int Rare = rs.getInt("Rare");
+				result = new HouseholdServlet(Number,Name,Type1,Type2,Rare);
+
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBCドライバが見つかりません。");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DBアクセス時にエラーが発生しました。");
+			e.printStackTrace();
+		} finally {
+			// ⑧DBMSから切断する
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+
+	public static Household deleteDao(int keyN){
+		//①準備
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Household result =null;
+
+
+		try {
+			// ②JDBCドライバをロードする
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// ③DBMSとの接続を確立する
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/dbdb?useSSL=false",
+					"root",
+					"1104");
+
+			// ④SQL文を作成する
+			String sql = "delete from pokemon where Number = ? ;";
+
+			// ⑤SQL文を実行するための準備を行う
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1,keyN);
+
+			// ⑥SQL文を実行してDBMSから結果を受信する
+			pstmt.executeUpdate();
+
+
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBCドライバが見つかりません。");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DBアクセス時にエラーが発生しました。");
+			e.printStackTrace();
+		} finally {
+			// ⑧DBMSから切断する
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public static int registerDao(int keyA,String key,String key1,String key2,String key3,String key4){
+		//①準備
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int result = 0;
+
+
+		try {
+			// ②JDBCドライバをロードする
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// ③DBMSとの接続を確立する
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/dbdb?useSSL=false",
+					"root",
+					"1104");
+
+			// ④SQL文を作成する
+			String sql = "insert into pokemon(Number,Name,Type1,Type2,Rare) VALUE(?,?,?,?,?)  ;";
+
+			// ⑤SQL文を実行するための準備を行う
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1,keyA);
+			pstmt.setString(2,key1);
+			pstmt.setString(3,key2);
+			pstmt.setString(4,key3);
+			pstmt.setString(5,key4);
+			result = pstmt.executeUpdate();
+
+			// ⑥SQL文を実行してDBMSから結果を受信する
+
+
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBCドライバが見つかりません。");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DBアクセス時にエラーが発生しました。");
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+
+
+
+
+
+}
